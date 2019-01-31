a8418242c7a8e7b8c54ac0811984e7da9c0dc94a
allaude <riyan.stmik28@gmail.com>
2019-01-31 22:54:57 +0700

    fixing post cost

--- mystore/api/resources/track.py
+++ mystore/api/resources/track.py
@@ -1,43 +1,79 @@
-from rajaongkir import RajaOngkirApi
 from flask import request
+import requests
 from flask_restful import Resource
-from flask_jwt_extended import jwt_required, get_jwt_identity
-
-from mystore.models import Item
-from mystore.extensions import ma, db
 
 
-class OngkirSchema(ma.ModelSchema):
-    api = RajaOngkirApi('182561962cf94cd52a5a013a6cc058fa')
+class APIParent():
+    type = "starter"
+    key = "182561962cf94cd52a5a013a6cc058fa"
+    url = 'https://api.rajaongkir.com/' + type + '/'
+    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8', 'key': key}
+    failed = {"msg": "service currently not available"}
+
+
+class Hello(Resource):
+    def get(self):
+        return {"msg": "Welcome to Store App"}
 
 
 class Province(Resource):
     def get(self):
-        schema = OngkirSchema()
-        api = schema.api
-        province = api.provinces()
-        return province
+        api = APIParent()
+        url = api.url + 'province'
+        try:
+            r = requests.get(url, headers=api.headers)
+            return r.json()
+        except requests.exceptions.RequestException as e:
+            return e
 
 
 class City(Resource):
     def get(self):
-        schema = OngkirSchema()
-        api = schema.api
-        cities = api.cities()
-        return cities
-
-
-class CityByProvince(Resource):
-    def get(self, province_id):
-        schema = OngkirSchema()
-        api = schema.api
-        cities = api.cities_by_province(province_id)
-        return cities
+        api = APIParent()
+        url = api.url + 'city'
+        try:
+            r = requests.get(url, headers=api.headers)
+            return r.json()
+        except requests.exceptions.RequestException as e:
+            return e
 
 
 class CityById(Resource):
     def get(self, city_id):
-        schema = OngkirSchema()
-        api = schema.api
-        cities = api.city_by_id(city_id)
-        return cities
+        api = APIParent()
+        url = api.url + 'city?id=' + city_id
+        try:
+            r = requests.get(url, headers=api.headers)
+            return r.json()
+        except requests.exceptions.RequestException as e:
+            return e
+
+
+class CityByProvince(Resource):
+    def get(self, province_id):
+        api = APIParent()
+        url = api.url + 'city?province=' + province_id
+        try:
+            r = requests.get(url, headers=api.headers)
+            return r.json()
+        except requests.exceptions.RequestException as e:
+            return e
+
+
+class Cost(Resource):
+    def post(self):
+        data = request.json
+        api = APIParent()
+        url = api.url + 'cost'
+        payload = "origin="+data['city_from']+"&destination="+data['city_from']+"&weight="+data['city_from']+"&courier="+data['courier']
+        try:
+            r = requests.post(url,  data=payload, headers={'content-type': 'application/x-www-form-urlencoded', 'key': api.key})
+            return r.json()
+        except requests.exceptions.RequestException as e:
+            return e
+
+
+class Courier(Resource):
+    def get(self):
+        data = {"courier1": "pos", "courier2": "tiki", "courier3": "jne"}
+        return data
--- mystore/api/views.py
+++ mystore/api/views.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 
 from mystore.api.resources.user import UserResource, UserList
 from mystore.api.resources.item import ItemResource, ItemList
-from mystore.api.resources.track import Province, City, CityById, CityByProvince
+from mystore.api.resources.track import Province, City, CityById, CityByProvince, Cost, Hello, Courier
 blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
 api = Api(blueprint)
 
@@ -14,5 +14,8 @@ api.add_resource(ItemResource, 'items/<int:item_id>')
 api.add_resource(ItemList, '/items')
 api.add_resource(Province, '/tracking/provinces')
 api.add_resource(City, '/tracking/cities')
-api.add_resource(CityById, '/tracking/city/<int:city_id>')
-api.add_resource(CityByProvince, '/tracking/city-by-province/<int:province_id>')
+api.add_resource(CityById, '/tracking/city/<string:city_id>')
+api.add_resource(CityByProvince, '/tracking/city-by-province/<string:province_id>')
+api.add_resource(Cost, '/tracking/cost')
+api.add_resource(Hello, '/')
+api.add_resource(Courier, '/tracking/courier')
